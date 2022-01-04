9d77cd3d16070a9172fd40873695d1b520c5c0e7
aaronespasa <aaespasa@gmail.com>
2022-01-04 23:56:41 +0100

    Creating the dataset file to prepare the data

---
+++ Dataset/Dataset.py
@@ -0,0 +1,32 @@
+"""
+Copyright (c), Aarón Espasandín - All Rights Reserved
+
+This source code is licensed under the BSD 3-Clause license found in the
+LICENSE file in the root directory of this source tree:
+https://github.com/aaronespasa/Wall-Painting/blob/main/LICENSE
+"""
+import tensorflow as tf
+
+from glob import glob
+import os
+import json
+from urllib.request import urlretrieve
+from zipfile import ZipFile
+
+from PIL import Image
+import cv2
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+class Dataset:
+    def __init__(self):
+        self.DATA_FOLDER_NAME = "data"
+        self.TRAINING_ODGT_PATH = os.path.join(self.DATA_FOLDER_NAME, "training.odgt")
+        self.VALIDATION_ODGT_PATH = os.path.join(self.DATA_FOLDER_NAME, "validation.odgt")
+
+        self.train_samples = [json.loads(x.rstrip()) for x in open(self.TRAINING_ODGT_PATH, 'r')]
+        self.val_samples = [json.loads(x.rstrip()) for x in open(self.VALIDATION_ODGT_PATH, 'r')]
+
+        self.SCENE_CATEGORIES_PATH = os.path.join(self.DATA_FOLDER_NAME, "ADEChallengeData2016", "sceneCategories.txt")
+
