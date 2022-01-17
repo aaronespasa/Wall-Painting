4efaaf433b2fe8f5a0819c9b0e53324523ae2147
aaronespasa <aaespasa@gmail.com>
2022-01-17 18:38:21 +0100

    Adding the list of scenes to the constants file

--- Dataset/constants.py
+++ Dataset/constants.py
@@ -16,3 +16,21 @@ VALIDATION_ODGT_PATH = os.path.join(DATA_FOLDER_NAME, "validation.odgt")
 SCENE_CATEGORIES_PATH = os.path.join(DATA_FOLDER_NAME,
                                     "ADEChallengeData2016",
                                     "sceneCategories.txt")
+
+SCENES_LIST = ['bathroom', 'bedroom', 'kitchen', 'living_room',
+               'art_gallery', 'art_studio', 'attic', 'auditorium',
+                'shop', 'ballroom', 'bank_indoor', 'banquet_hall',
+                'bar', 'basement', 'bookstore', 'childs_room',
+                'classroom', 'room', 'closet', 'clothing_store',
+                'computer_room', 'conference_room', 'corridor',
+                'office', 'darkroom', 'dentists_office', 'diner_indoor',
+                'dinette_home', 'dining_room', 'doorway_indoor',
+                'dorm_room', 'dressing_room', 'entrance_hall', 'galley',
+                'game_room', 'garage_indoor', 'gymnasium_indoor',
+                'hallway', 'home_office', 'hospital_room', 'hotel_room',
+                'jail_cell', 'kindergarden_classroom',
+                'lecture_room', 'library_indoor', 'lobby', 'museum_indoor',
+                'nursery', 'playroom', 'staircase',
+                'television_studio', 'utility_room', 'waiting_room',
+                'warehouse_indoor', 'youth_hostel']
+
